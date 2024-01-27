dac74440986628b7d414696f01990f2423ba3a93
majg8a <majg8a@gmail.com>
2024-01-26 21:42:43 -0400

    feat: character direction animation

--- ghost/ghost.gd
+++ ghost/ghost.gd
@@ -2,10 +2,14 @@ extends enemy
 
 var walkInstance: walk = walk.new()
 var animations: Dictionary = {
-	Vector2.UP: self.animations"up",
-	Vector2.RIGHT: "right",
-	Vector2.DOWN: "down",
-	Vector2.LEFT: "left"
+	Vector2.UP: func ():
+		self.animatedSprite2D.play("up"),
+	Vector2.RIGHT: func ():
+		self.animatedSprite2D.play("right"),
+	Vector2.DOWN: func ():
+		self.animatedSprite2D.play("down"),
+	Vector2.LEFT: func ():
+		self.animatedSprite2D.play("left")
 }
 
 func behavior():
