1e166e04773dfc317ee6acd821dcedefcf28e23d
majg8a <majg8a@gmail.com>
2024-01-27 03:32:47 -0400

    fix: apply no, call yes

---
+++ ghost/ghost.gd
@@ -0,0 +1,22 @@
+extends enemy
+
+var walkInstance: walk = walk.new()
+
+func _ready():
+	super._ready()
+	var onDirectionChange = func (direction):
+		self.animations[direction].call()
+	directionSignal.connect(onDirectionChange)
+	
+	
+func behavior():
+	super.behavior()
+	var behaviors: Dictionary = {
+		BEHAVIOR.IDLE: func ():
+			directionSignal.emit(walkInstance.random(self)),
+		BEHAVIOR.AGGRESSIVE: func ():
+			pass
+	}
+	
+	behaviors[self.currentBehavior]
+	
