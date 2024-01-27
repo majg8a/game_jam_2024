458ce07db568ed31e9f35f2da9a7c578a0b8216b
majg8a <majg8a@gmail.com>
2024-01-26 22:24:27 -0400

    fix: entity area2d

--- ghost/ghost.gd
+++ ghost/ghost.gd
@@ -1,18 +1,11 @@
 extends enemy
 
 var walkInstance: walk = walk.new()
-var animations: Dictionary = {
-	Vector2.UP: func ():
-		self.animatedSprite2D.play("up"),
-	Vector2.RIGHT: func ():
-		self.animatedSprite2D.play("right"),
-	Vector2.DOWN: func ():
-		self.animatedSprite2D.play("down"),
-	Vector2.LEFT: func ():
-		self.animatedSprite2D.play("left")
-}
+
+func _ready():
+	super._ready()
 
 func behavior():
 	super.behavior()
 	var direction = walkInstance.random(self)
-	
+	self.animations[direction].apply()
