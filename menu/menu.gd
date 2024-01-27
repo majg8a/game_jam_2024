1adf8dd39de5062df1c269f5ce5528e4cc846a08
edavila11 <erick.avila@airtek.com.ve>
2024-01-27 17:24:04 -0400

    adding image to menu and fix buttons menu

--- menu/menu.gd
+++ menu/menu.gd
@@ -4,28 +4,30 @@ extends Control
 @onready var options_button = $VBoxContainer/options_button
 @onready var quit_button = $VBoxContainer/quit_button
 @onready var margin_container = $options_menu/MarginContainer
-@onready var vbox_container = $VBoxContainer as VBoxContainer
+@onready var vbox_container = $VBoxContainer
+@onready var exit_button = $options_menu/MarginContainer/VBoxContainer/Exit_Button
 
-# Called when the node enters the scene tree for the first time.
 func _ready():
-	$VBoxContainer/start_button.grab_focus()
+	start_button.grab_focus()
 	handle_connecting_signals()
 	
 func on_start_button_pressed():
-	get_tree().change_scene() #ingresar aca direccion del primer nivel o intro video
+	get_tree().change_scene_to_file("res://test.tscn")
 
 func on_options_button_pressed():
 	vbox_container.visible = false
 	margin_container.set_process(true)
 	margin_container.visible = true
-
+	exit_button.grab_focus()
+	
 func on_quit_button_pressed():
-	get_tree().quit
+	get_tree().quit()
 	
 func _on_exit_button_pressed():
 	vbox_container.visible = true
 	margin_container.set_process(false)
 	margin_container.visible = false
+	start_button.grab_focus()
 	
 func handle_connecting_signals() -> void:
 	start_button.button_down.connect(on_start_button_pressed)
---
+++ singletons/dialogsManager.gd
@@ -0,0 +1,11 @@
+enum DIALOGS {
+	INTRO,
+	DEAD,
+}
+
+var dialogs_dict = {
+	DIALOGS.INTRO:"HOLA DESDE MI INTRO"
+}
+
+
+
