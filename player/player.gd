f3b09397334bb51cbdb187c10fbafc2b8e067f2b
majg8a <majg8a@gmail.com>
2024-01-28 00:06:48 -0400

    feat: player movement animation

--- player/player.gd
+++ player/player.gd
@@ -9,11 +9,11 @@ func _init():
 		Vector2.UP: func ():
 			self.animatedSprite2D.play("move_up"),
 		Vector2.RIGHT: func ():
-			self.animatedSprite2D.play("move_right"),
+			self.animatedSprite2D.play("move_side"),
 		Vector2.DOWN: func ():
 			self.animatedSprite2D.play("move_down"),
 		Vector2.LEFT: func ():
-			self.animatedSprite2D.play("move_left"),
+			self.animatedSprite2D.play("move_side"),
 		Vector2.ZERO: func ():
 			self.animatedSprite2D.play("idle")
 	}
@@ -21,24 +21,29 @@ func _init():
 var movementAtions: Dictionary = {
 	"ui_up": 
 		func ():
+			animations[Direction.directions[0]].call()
 			self.move_and_collide(self.speed * Direction.directions[0]),
 	"ui_right": 
 		func ():
+			animations[Direction.directions[1]].call()
 			self.move_and_collide(self.speed * Direction.directions[1]),
 	"ui_down":  
 		func ():
+			animations[Direction.directions[2]].call()
 			self.move_and_collide(self.speed * Direction.directions[2]),
 	"ui_left": 
 		func ():
+			animations[Direction.directions[3]].call()
 			self.move_and_collide(self.speed * Direction.directions[3]),
 }
-
-func _process(delta):
-	self.camera2D.position = self.position
 
 func behavior():
 	if currentState == STATE.ALIVE:
 		for key in self.movementAtions.keys():
 			if Input.is_action_pressed(key):
+				if key == "ui_right":
+					self.animatedSprite2D.flip_h = true
+				else:
+					self.animatedSprite2D.flip_h = false
 				self.movementAtions[key].call()
 				
